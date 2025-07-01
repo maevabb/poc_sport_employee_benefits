7ae17e2abc08629d7d41b0c758e672c6eec44572
Maeva Beauvillain <m.berte@hotmail.fr>
2025-07-01 17:09:56 +0200

    Create expectation suite for activities_sportives table

--- scripts/great_expectations/gx_create_suite.py
+++ scripts/great_expectations/gx_create_suite.py
@@ -4,13 +4,34 @@ context = gx.get_context()
 
 suite_name = "validate_activities_sportives"
 suite = gx.ExpectationSuite(name=suite_name)
+
 suite = context.suites.add(suite)
 
+# Colonnes non-nulles
+suite.add_expectation(gx.expectations.ExpectColumnValuesToNotBeNull(column="id_employe"))
+suite.add_expectation(gx.expectations.ExpectColumnValuesToNotBeNull(column="debut_activite"))
+suite.add_expectation(gx.expectations.ExpectColumnValuesToNotBeNull(column="fin_activite"))
+suite.add_expectation(gx.expectations.ExpectColumnValuesToNotBeNull(column="type_activite"))
+suite.add_expectation(gx.expectations.ExpectColumnValuesToNotBeNull(column="temps_sec"))
 
-notnull_expectation = gx.expectations.ExpectColumnValuesToNotBeNull(
-    column="id_employe"
-)
+# Distance et durée positives
+suite.add_expectation(gx.expectations.ExpectColumnMaxToBeBetween(
+    column="distance",
+    min_value= 0,
+))
+suite.add_expectation(gx.expectations.ExpectColumnMaxToBeBetween(
+    column="temps_sec",
+    min_value= 0,
+))
 
-suite.add_expectation(notnull_expectation)
+# Date de fin >= Date début
+suite.add_expectation(gx.expectations.ExpectColumnPairValuesAToBeGreaterThanB(
+    column_A="debut_activite",
+    column_B="fin_activite",
+    or_equal=True
+))
+
+
+
 
 
